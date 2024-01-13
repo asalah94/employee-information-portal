511d1739f54d6ee847417629aebc7f99c9f6f661
Ahmed Salah <asalah.c@saip.gov.sa>
2024-01-13 12:34:15 -0800

    third commit

---
+++ src/test/java/information/portal/SqlitedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package information.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlitedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
